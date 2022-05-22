2981cb0bfd881b29a2a9b3940b961237fc1e90cb
jicjjang <jicjjang12@gmail.com>
2022-05-23 01:17:58 +0900

    Append array example.

---
+++ src/arr.ts
@@ -0,0 +1,7 @@
+const arrValue1: Array<string> = ["1", "2", "3"];
+const arrValue2: string[] = ["1", "2", "3"];
+arrValue1.pop();
+
+const arrValue3: ReadonlyArray<string> = ["1", "2", "3"];
+arrValue3.pop();
+arrValue3.push("4");
