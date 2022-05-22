9ea11e91c021918f4d8c7ddfe0216991aa385172
jicjjang <jicjjang12@gmail.com>
2022-05-22 22:41:19 +0900

    Append string-number-boolean codes

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
-const version = process.version;
-const splitedVersion: number[] = process.version.split('.');
+const version: string = process.version; // 'v16.15.0'
+const splitedVersion: string[] = process.version.split("."); // ['v16', '15', '0']
+const splitedNumberVersion: number[] = splitedVersion.map((eachValue: string, index: number) =>
+  parseInt(!index ? eachValue.substring(1) : eachValue, 10)
+);
 
-console.log(splitedVersion);
+const isNumArr: boolean =
+  Array.isArray(splitedNumberVersion) &&
+  splitedNumberVersion.every((eachValue: number) => typeof eachValue === "number");
+
+console.log(splitedNumberVersion); // [16, 15, 0]
+console.log(isNumArr); // true
